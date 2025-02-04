87de824a3ca6c5af283a30d2e88005513740b9bc
adachi0612 <adachi0612@docomo.ne.jp>
2025-02-04 14:00:13 +0900

    modified

--- iris.py
+++ iris.py
@@ -1,12 +1,19 @@
-import pandas as pd
-import numpy as np
-import mglearn
 import matplotlib.pyplot as plt
+import mglearn
+import numpy as np
+import pandas as pd
 import seaborn as sns
 from sklearn.datasets import load_iris
 
-iris = load_iris()
 
 class AnalyzeIris:
-    def __init__(self, iris):
+    def __init__(self):
+        # load_iris()でIrisデータセットを読み込む
+        self.iris = load_iris()
 
+    def get(self):
+        # データフレームを作成
+        iris_data = pd.DataFrame(self.iris.data, columns=self.iris.feature_names)
+        # ラベルを追加
+        iris_data["Label"] = self.iris.target
+        return iris_data
