5f5db86d9b09bb8f2d7599449bee2eb987203325
adachi0612 <adachi0612@docomo.ne.jp>
2025-02-04 13:41:13 +0900

    test

---
+++ iris.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import mglearn
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+
+class AnalyzeIris:
+    def __init__(self, iris):
+
