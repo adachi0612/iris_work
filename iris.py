4e92ce7848338b5490c4ff8bbaddfb9984e343ba
adachi0612 <adachi0612@docomo.ne.jp>
2025-02-05 22:26:38 +0900

    studied

--- iris.py
+++ iris.py
@@ -1,9 +1,20 @@
+# 可視化ライブラリを読み込む
+import graphviz
 import matplotlib.pyplot as plt
 import mglearn
 import numpy as np
 import pandas as pd
 import seaborn as sns
+
+# 教師あり学習の各手法を読み込む
 from sklearn.datasets import load_iris
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import KFold, cross_validate
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 
 
 class AnalyzeIris:
@@ -11,36 +22,132 @@ class AnalyzeIris:
         # load_iris()でIrisデータセットを読み込む
         self.iris = load_iris()
         # 正解ラベルのない各特徴量のデータフレームを作成
-        self.iris_data = pd.DataFrame(self.iris.data, columns=self.iris.feature_names)
+        self.iris_dataframe = pd.DataFrame(
+            self.iris.data, columns=self.iris.feature_names
+        )
 
     def get(self):
-        self.iris_data_label = pd.DataFrame(
+        self.iris_dataframe_label = pd.DataFrame(
             self.iris.data, columns=self.iris.feature_names
         )
         # ラベルの列を追加
-        self.iris_data_label["Label"] = self.iris.target
-        return self.iris_data_label
+        self.iris_dataframe_label["Label"] = self.iris.target
+        return self.iris_dataframe_label
 
     def get_correlation(self):
         # 各特徴量間の相関係数を求める
-        return self.iris_data.corr()
+        return self.iris_dataframe.corr()
 
     def pair_plot(self, diag_kind="hist"):
         # 正解ラベルのない各特徴量のデータフレームを作成
-        self.iris_data_species = pd.DataFrame(
+        self.iris_dataframe_species = pd.DataFrame(
             self.iris.data, columns=self.iris.feature_names
         )
         # 種の正解ラベル列を追加
-        self.iris_data_species["Species"] = np.array(
+        self.iris_dataframe_species["Species"] = np.array(
             self.iris.target_names[i] for i in self.iris.target
         )
         # seabornでペアプロットを行う
         return sns.pairplot(
-            self.iris_data_species,
+            self.iris_dataframe_species,
             hue="Species",
             diag_kind=diag_kind,
             markers="o",
             palette="tab10",
         )
 
-    # def all_supervised(self, n_neighbors=4):
+    def all_supervised(self, n_neighbors=4):
+        model = [
+            LogisticRegression(),
+            LinearSVC(),
+            SVC(),
+            DecisionTreeClassifier(),
+            KNeighborsClassifier(n_neighbors=n_neighbors),
+            LinearRegression(),
+            RandomForestClassifier(),
+            GradientBoostingClassifier(),
+            MLPClassifier(),
+        ]
+        model_name = [
+            "LogisticRegression",
+            "LinearSVC",
+            "SVC",
+            "DecisionTreeClassifier",
+            "KNeighborClassifier",
+            "LinearRegression",
+            "RandomForestClassifier",
+            "GradientBoostingClassifier",
+            "MLPClassifier",
+        ]
+        # 5分割交差検証
+        kfold = KFold(n_splits=5)
+        # iris.get_supervised()での出力結果のために空のデータフレームを作る
+        self.iris_supervised = pd.DataFrame()
+        self.supervised_model = []
+
+        for model, model_name in zip(model, model_name):
+            print("=== {} ===".format(model_name))
+            score = cross_validate(
+                model,
+                self.iris.data,
+                self.iris.target,
+                cv=kfold,
+                return_train_score=True,
+                return_estimator=True,
+            )
+            # テストセットの分類精度で最も性能の良かった学習した分類器をリストとして保持しておく
+            self.supervised_model.append(
+                score["estimator"][np.argmax(score["test_score"])]
+            )
+            self.iris_supervised[model_name] = score["test_score"]
+            for i in range(5):
+                print(
+                    "test score:{:.3f}, train score:{:.3f}".format(
+                        score["test_score"][i], score["train_score"][i]
+                    )
+                )
+
+    def get_supervised(self):
+        return self.iris_supervised
+
+    def best_supervised(self):
+        # 行と列を入れ替え、インデックスを列に変更
+        iris_supervised_T = self.iris_supervised.describe().T
+        best_score = iris_supervised_T["mean"].max()
+        max_method = iris_supervised_T["mean"].idxmax()
+        return max_method, best_score
+
+    def plot_feature_importances_all(self):
+        tree_model = [
+            "DecisionTreeClassifier",
+            "RandomForest",
+            "GradientBoostingClassifier",
+        ]
+        # tree_modelに入っている学習済み分類器はself.supervised_modelの3,6,7番目の要素
+        for model, model_name in zip(
+            [
+                self.supervised_model[3],
+                self.supervised_model[6],
+                self.supervised_model[7],
+            ],
+            tree_model,
+        ):
+            plt.figure()
+            plt.barh(
+                range(len(self.iris.feature_names)),
+                model.feature_importances_,
+                align="center",
+            )
+            plt.yticks(np.arange(len(self.iris.feature_names)), self.iris.feature_names)
+            plt.xlabel("Feature importance:{}".format(model_name))
+
+    def visualize_decision_tree(self):
+        dot_data = export_graphviz(
+            self.supervised_model[3],
+            out_file=None,
+            feature_names=self.iris.feature_names,
+            class_names=self.iris.target_names,
+            filled=True,
+            impurity=False,
+        )
+        return graphviz.Source(dot_data)
